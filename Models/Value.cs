563fbeb2a64e1d5bdc65a1ace6bbc6eed22ddbd7
guiver <guiver95@gmail.com>
2019-06-09 20:54:05 -0400

    test migration

--- Models/Value.cs
+++ Models/Value.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace RestApiDating.Models
 {
     public class Value
     {
         public int Id {get; set;}
         public string Name { get; set; }
+        public DateTime Date {get; set;} = DateTime.Now;
     }
 }

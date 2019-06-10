f9017407421273e66be8fd40948626e1b9a97fd8
guiver <guiver95@gmail.com>
2019-06-09 20:05:17 -0400

    initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using RestApiDating.Models;
+
+namespace RestApiDating.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+        : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}

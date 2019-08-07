2a390cc72b6692cf6c349d91cfb8af3f1f04e936
guiver <guiver95@gmail.com>
2019-08-07 15:22:42 -0400

    code cleanup

--- Helpers/AutomapperProfiles.cs
+++ Helpers/AutomapperProfiles.cs
@@ -11,26 +11,35 @@ namespace RestApiDating.Helpers
         public AutomapperProfiles()
         {
             CreateMap<User, UserListDto>()
-                .ForMember(dest => dest.FotoUrl, opt =>
-                {
-                    // MapFrom para especificar la propiedad desde el source
-                    opt.MapFrom(src => src.Fotos.FirstOrDefault(f => f.EsPrincipal).Url);
-                })
-                .ForMember(dest => dest.Edad, opt =>
-                {
-                    // ResolveUsing para propiedades calculadas
-                    opt.ResolveUsing(src => src.FechaNacimiento.CalcularEdad());
-                });
+                .ForMember(dest => dest.FotoUrl, opt => opt.MapFrom(src => PrincipalFotoUrl(src)))
+                .ForMember(dest => dest.Edad, opt => opt.MapFrom(src => CalcularEdad(src)));
             CreateMap<User, UserDetailDto>()
-                .ForMember(dest => dest.FotoUrl, opt =>
-                {
-                    opt.MapFrom(src => src.Fotos.FirstOrDefault(f => f.EsPrincipal).Url);
-                })
-                .ForMember(dest => dest.Edad, opt =>
-                {
-                    opt.ResolveUsing(src => src.FechaNacimiento.CalcularEdad());
-                });
+                .ForMember(dest => dest.FotoUrl, opt => opt.MapFrom(src => PrincipalFotoUrl(src)))
+                .ForMember(dest => dest.Edad, opt => opt.MapFrom(src => CalcularEdad(src)));
             CreateMap<Foto, FotoDto>();
         }
+
+        #region custom mappings
+        private string PrincipalFotoUrl(User user)
+        {
+            return user.Fotos.FirstOrDefault(f => f.EsPrincipal).Url;
+        }
+
+        private int CalcularEdad(User user)
+        {
+            var fechaNacimiento = user.FechaNacimiento;
+            if (fechaNacimiento != null)
+            {
+                var edad = DateTime.Today.Year - fechaNacimiento.Value.Year;
+                if (fechaNacimiento.Value.AddYears(edad) > DateTime.Today)
+                {
+                    edad--;
+                } 
+                return edad;
+            }
+
+            return 0;
+        }
+        #endregion
     }
 }
